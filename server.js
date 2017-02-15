8b1aa1d89973cea838e457b28ad53998f4476582
Adrian N <adrian.c.nedelcu@gmail.com>
2017-02-15 19:04:12 +0200

    Show timestampon UI

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-//var moment = require('moment');
-//var now = moment();
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -14,13 +13,17 @@ io.on('connection', function (socket) {
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
         
+        message.timestamp = moment().valueOf();
         //io.emit catre toata lumea incluzandu-l pe cel care trimite
         //socket.broadcast.emit catre toata lumea excluzandu-l pe cal care trimite
-        io.emit(/*now.format('Do-MMM-YYYY h:mm a') + */'message', message);
+        io.emit('message', message);
     });
     
+    var timpestamp = 
+    
     socket.emit('message', {
-        text: 'Welcome to the chat application of the Pack!'
+        text: 'Welcome to the chat application of the Pack!',
+        timestamp: moment().valueOf()
     });
 });
 
