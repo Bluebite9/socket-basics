bf959f1e0882da76e793c457db76d45f1bfacc21
Adrian N <adrian.c.nedelcu@gmail.com>
2017-02-20 11:56:51 +0200

    Add support for chat rooms

--- server.js
+++ server.js
@@ -7,8 +7,22 @@ var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {
+    
+};
+
 io.on('connection', function (socket) {
     console.log('User connected via socket.io');
+    
+    socket.on('joinRoom', function (req) {
+        clientInfo[socket.id] = req;
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message', {
+            name: 'System',
+            text: req.name + ' has joined!',
+            timestamp: moment().valueOf()
+        });
+    });
     
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
@@ -16,7 +30,7 @@ io.on('connection', function (socket) {
         message.timestamp = moment().valueOf();
         //io.emit catre toata lumea incluzandu-l pe cel care trimite
         //socket.broadcast.emit catre toata lumea excluzandu-l pe cal care trimite
-        io.emit('message', message);
+        io.to(clientInfo[socket.id].room).emit('message', message);
     });
     
     var timpestamp = 
